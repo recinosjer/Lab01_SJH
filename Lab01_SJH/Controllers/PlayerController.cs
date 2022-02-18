a0e678b6641b8ca1b86ed3a4b4e3dd6f9d79e771
SAMER ARANKI MUNOZ <saranki@correo.url.edu.gt>
2022-02-17 20:15:38 -0600

    correcion de comit anterior, (no lo guarde bien)

--- Lab01_SJH/Controllers/PlayerController.cs
+++ Lab01_SJH/Controllers/PlayerController.cs
@@ -1,12 +1,74 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lab01_SJH.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Lab01_SJH.Controllers
 {
     public class PlayerController : Controller
     {
+        List<Player> playersList = new List<Player>();
+
         public IActionResult Index()
         {
+            List<Teams> teamsList = new List<Teams>();
+            List<Player> playersList = new List<Player>();
+
+            // Ejemplo incerscion manual
+            playersList.Add(new Player()
+            {
+                Name = "Faker",
+                LName = "GoJeonPa",
+                Team = "T1",
+                Rol = "mid-laner",
+                KDA = 4.3,
+                CS = 846
+            });
+
             return View();
+        }
+
+        [Route("SubirArchivo")]
+        public IActionResult subirArchivos()
+        {
+            return View();
+        }
+
+
+        [HttpPost("SubirArchivo")]
+        public IActionResult subirArchivos(IFormFile file)
+        {
+            if (file != null)
+            {
+                try
+                {
+                    string ruta = Path.Combine(Path.GetTempPath(), file.Name);
+                    using (var stream = new FileStream(ruta, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    string allFileData = System.IO.File .ReadAllText(ruta);
+                    foreach (string lineaActual in allFileData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(lineaActual))
+                        {
+                            string[] informacion = lineaActual.Split(',');
+                            var player = new Player();
+                            player.Name = informacion[0];
+                            player.LName = informacion[1];
+                            player.Team = informacion[2];
+                            player.Rol = informacion[3];
+                            player.KDA = Convert.ToDouble(informacion[4]);
+                            player.CS = Convert.ToInt32(informacion[5]);
+                            playersList.Add(player);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    ViewBag.Error = e.Message;
+                }
+            }
+            return View(playersList);
         }
     }
 }
--- Lab01_SJH/Models/Player.cs
+++ Lab01_SJH/Models/Player.cs
@@ -4,7 +4,7 @@ namespace Lab01_SJH.Models
     public class Player : Coach
     {
         public string Rol { get; set; }
-        public decimal KDA { get; set; }
+        public double KDA { get; set; }
         public int CS { get; set; }
     }
 }
