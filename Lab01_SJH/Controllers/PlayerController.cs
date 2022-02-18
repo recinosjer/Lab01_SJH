43ab7de63912c4a30749418a2a50c52893ae4971
SAMER ARANKI MUNOZ <saranki@correo.url.edu.gt>
2022-02-17 20:14:43 -0600

    PLayerControler, lectura de archivo, ejemplo de incersion manual

---
+++ Lab01_SJH/Controllers/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab01_SJH.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
